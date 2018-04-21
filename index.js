c532d67a1fac24ba9ece6060f7c481fa6aa8cdcc
spike yang <ganioc.yang@gmail.com>
2018-04-21 16:54:14 +0800

    try get markethistorykline

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ var path = require("path");
 var HuobiClient = require("./lib/huobi/HuobiClient.js");
 var utils = new Utils(path.basename(__filename));
 
+utils.printGray("********************************************");
+utils.printGray("|\\     /||\\     /|(  ___  )(  ___ \\ \\__   __/");
+utils.printGray("| )   ( || )   ( || (   ) || (   ) )   ) (");
+utils.printGray("| (___) || |   | || |   | || (__/ /    | |");
+utils.printGray("|  ___  || |   | || |   | ||  __ (     | | ");
+utils.printGray("| (   ) || |   | || |   | || (  \\ \\    | |");
+utils.printGray("| )   ( || (___) || (___) || )___) )___) (___");
+utils.printGray("|/     \\|(_______)(_______)|/ \\___/ \\_______/");
+utils.printGray("*********************************************");
+
 var client = new HuobiClient();
 
-utils.printGray();
-utils.printGray();
-
+utils.printYellow("Try to get marketHistoryKLine, 1min, 3, btcusdt");
 client.getMarketHistoryKLine("1min", "3", "btcusdt", (err, data) => {
   if (err) {
     utils.printRed("error:", err);
@@ -20,7 +28,9 @@ client.getMarketHistoryKLine("1min", "3", "btcusdt", (err, data) => {
     obj = JSON.parse(data);
   } catch (e) {
     utils.printRed(e);
+    return;
   }
+  utils.printGreen("Got response:");
   utils.print(JSON.parse(obj));
 });
 
