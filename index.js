e7c52032e7b557ce99ad97ce17940bdabac6b6ff
spike yang <ganioc.yang@gmail.com>
2018-04-21 15:36:59 +0800

    tune color display

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 "use strict";
 var Utils = require("./lib/utils.js");
+var path = require("path");
 
 var HuobiClient = require("./lib/huobi/HuobiClient.js");
-
-Utils.printGreen("hello huobi");
+var utils = new Utils(path.basename(__filename));
 
 var client = new HuobiClient();
 
-client.getTry();
+utils.printGray();
+utils.printGray();
+
+client.getMarketHistoryKLine("1min", "3", "btcusdt", (err, data) => {
+  if (err) {
+    utils.printRed("error:", err);
+    return;
+  }
+  var obj;
+  try {
+    obj = JSON.parse(data);
+  } catch (e) {
+    utils.printRed(e);
+  }
+  utils.print(JSON.parse(obj));
+});
+
+// utils.print("gray");
+// utils.printCyan("cyan");
